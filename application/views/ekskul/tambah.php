765a02ddb3b1ddf412f27db0f5ce4ac8805106cb
rahmrny12 <rahmatprayogo12@gmail.com>
2022-08-15 18:18:52 +0700

    upgrade bootstrap

---
+++ application/views/ekskul/tambah.php
@@ -0,0 +1,60 @@
+<div class="page-title">
+    <div class="title_left">
+        <h3>Tambah Ekstrakurikuler</h3>
+    </div>
+</div>
+<div class="clearfix"></div>
+
+<div class="row">
+    <div class="col-md-12 col-sm-12">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2>Tambah Ekstrakurikuler Baru</h2>
+                <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Settings 1</a>
+                            <a class="dropdown-item" href="#">Settings 2</a>
+                        </div>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+                <form action="<?= base_url('ekskul/tambah') ?>" method="post" novalidate>
+                    <div class="field item form-group">
+                        <label class="col-form-label col-md-3 col-sm-3  label-align" for="nama_ekskul">Nama Ekstrakurikuler<span class="required">*</span></label>
+                        <div class="col-md-6 col-sm-6">
+                            <input type="text" class="form-control has-feedback-left" id="nama_ekskul" name="nama_ekskul" placeholder="Masukkan nama ekskul">
+                            <label class="fa fa-wheelchair-alt form-control-feedback left" for="nama_ekskul" aria-hidden="true"></label>
+                        </div>
+                    </div>
+                    <div class="field item form-group">
+                        <label class="col-form-label col-md-3 col-sm-3  label-align" for="deskripsi">Deskripsi<span class="required">*</span></label>
+                        <div class="col-md-6 col-sm-6">
+                            <textarea class="col" id="deskripsi" name="deskripsi" placeholder="Masukkan deskripsi ekskul"></textarea>
+                        </div>
+                    </div>
+                    <div class="field item form-group">
+                        <label class="col-form-label col-md-3 col-sm-3  label-align" for="deskripsi">Deskripsi<span class="required">*</span></label>
+                        <div class="col-md-6 col-sm-6">
+                            <input class="form-control" type="file" id="formFile">
+                        </div>
+                    </div>
+                    <div class="ln_solid">
+                        <div class="form-group">
+                            <div class="col-md-6 offset-md-3 mt-4">
+                                <button type='submit' class="btn btn-secondary">Tambah Ekskul Baru</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
